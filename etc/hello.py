b49d807182a59bb1eedc8f12b227fa457934051d
Antonenko Timofey <antonenkodev@gmail.com>
2016-05-14 23:42:37 +0300

    change hello_2

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,9 @@
-CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    # 'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8080',
-        '--daemon',
-        '--workers=2',
-        '--timeout=60',
-        'hello:app',
-    ),
-}
+def wsgi_application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    body = environ['QUERY_STRING'].split('&')
+    start_response(status, headers)
+    resp = ''
+    for elem in body:
+        resp += (elem + '\n')
+    return resp
